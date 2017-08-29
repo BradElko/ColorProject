6e8172cb17574d533eb443b5a7eaa4cab5561078
BradElko <Uvozi@yahoo.com>
2017-08-29 14:05:41 -0400

    Semi Final

--- ColorProject2/Game.js
+++ ColorProject2/Game.js
@@ -1,3 +1,5 @@
+//Global Variables
+var Timer;
 var seconds;
 var accurateclicks;
 var inaccurateclicks;
@@ -5,71 +7,62 @@ var windowheight = window.innerHeight;
 var windowwidth = window.innerWidth;
 var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
 var randomcolor = colors[Math.floor(Math.random()*colors.length)];
-
+//BeginGameLoad Function
 function BeginGameLoad(){
-    var BottomLabel1 = document.getElementById("bl1");
+    //Variables
     var BeginGameContainer = document.getElementById("BGC1");
-    var RightClick = false;
+    var BottomLabel1 = document.getElementById("bl1");
+    //Events
     BottomLabel1.addEventListener("mousedown", BL1MD);
     BottomLabel1.addEventListener("mouseup", BL1MU);
-    BottomLabel1.style.cursor = "pointer";
-    window.addEventListener("resize", BeginGameResize);
     window.addEventListener("contextmenu", BeginGameContextMenu);
+    window.addEventListener("resize", BeginGameResize);
+    //If Window Height is less then or equal to 600...
+    if(windowheight <= 600){
+        BeginGameContainer.style.height = "600px";
+    }
+    //If Window Width is less then or equal to 800...
+    if(windowwidth <= 800){
+        BeginGameContainer.style.width = "800px";
+    }
+    //BL1MD Function
+    function BL1MD(e){
+        if(e.which == 1 || e.button == 0){
+            var randomcolor = colors[Math.floor(Math.random()*colors.length)];
+            BottomLabel1.style.color = randomcolor;
+        }
+    }
+    //BL1MU Function
+    function BL1MU(e){
+        if(e.which == 1 || e.button == 0){
+            window.location.href = "Game.html";
+            GameLoad();
+        }
+    }
+    //BeginGameContextMenu Function
     function BeginGameContextMenu(e){
         if(e.which == 3 || e.button == 2){
             e.preventDefault();
         }
     }
-    function BL1MD(e){
-        if(e.which == 3 || e.button == 2){
-            e.preventDefault();
-            RightClick = true;
-        }
-        else{
-            var randomcolor = colors[Math.floor(Math.random()*colors.length)];
-            BottomLabel1.style.color = randomcolor;
-            RightClick = false;
-        }
-    }
-    function BL1MU(){
-        if(RightClick == false){
-            window.location.href = "Game.html";
-            GameLoad();
-        }
-        else{
-            RightClick = false;
-        }
-    }
-    if(windowheight < 600)
-    {
-        BeginGameContainer.style.height = "600px";
-    }
-    if(windowwidth < 800)
-    {
-        BeginGameContainer.style.width = "800px";
-    }
+    //BeginGameResize Function
     function BeginGameResize(){
         windowheight = window.innerHeight;
         windowwidth = window.innerWidth;
-        if(windowheight <= 600)
-        {
+        if(windowheight <= 600){
             BeginGameContainer.style.height = "600px";
-        }
-        if(windowheight > 600)
-        {
+        }else if(windowheight > 600){
             BeginGameContainer.style.height = "100%";  
         }
-        if(windowwidth <= 800)
-        {
+        if(windowwidth <= 800){
             BeginGameContainer.style.width = "800px";
-        }
-        if(windowwidth > 800)
-        {
+        }else if(windowwidth > 800){
             BeginGameContainer.style.width = "100%";  
         }
     }
 }
 function GameLoad(){
+    //Variables
     var LeftPanel2 = document.getElementById("lp2");
     var LeftColorPanel2 = document.getElementById("lcp2");
     var TopPanel2 = document.getElementById("tp2");
@@ -84,25 +77,29 @@ function GameLoad(){
     var clickedRightName = false;
     var clickedRightColor = false;
     var clickedRightNameColor = false;
-    var RightClick = false;
     var ClickedLeftColorPanel = false;
     var ClickedRightLabel = false;
     var ClickedTopLabel = false;
-    var Timer;
+    //Events
     TopPanel2.addEventListener("mouseup", TP2MU);
     TopLabel2.addEventListener("mousedown", TL2MD);
     TopLabel2.addEventListener("mouseup", TL2MU);
-    TopLabel2.style.cursor = "pointer";
     LeftPanel2.addEventListener("mouseup", LP2MU);
     LeftColorPanel2.addEventListener("mouseup", LCP2MU);
     LeftColorPanel2.addEventListener("mousedown", LCP2MD);
-    LeftColorPanel2.style.cursor = "pointer";
     RightPanel2.addEventListener("mouseup", RP2MU);
     RightLabel2.addEventListener("mouseup", RL2MU);
     RightLabel2.addEventListener("mousedown", RL2MD);
-    RightLabel2.style.cursor = "pointer";
     window.addEventListener("resize", GameResize);
     window.addEventListener("contextmenu", GameContextMenu);
+    //If Window Height is less then or equal to 600...
+    if(windowheight <= 600){
+        GameContainer.style.height = "600px";
+    }
+    //If Window Width is less then or equal to 800...
+    if(windowwidth <= 800){
+        GameContainer.style.width = "800px";
+    }
     function AddSeconds(){
         seconds++;
     }
@@ -263,14 +260,6 @@ function GameLoad(){
             ClickedLeftColorPanel = false;
         }
     }
-    if(windowheight < 600)
-    {
-        GameContainer.style.height = "600px";
-    }
-    if(windowwidth < 800)
-    {
-        GameContainer.style.width = "800px";
-    }
     function GameResize(){
         windowheight = window.innerHeight;
         windowwidth = window.innerWidth;
@@ -293,6 +282,7 @@ function GameLoad(){
     }
 }
 function EndGameLoad(){
+    //Variables
     var BottomLabel3 = document.getElementById("bl3");
     var EndGameContainer = document.getElementById("EGC3");
     var MiddleLabelTwo = document.getElementById("mltwo3");
@@ -302,68 +292,58 @@ function EndGameLoad(){
     var seconds = localStorage.getItem("seconds");
     var accurateclicks = localStorage.getItem("accurateclicks");
     var inaccurateclicks = localStorage.getItem("inaccurateclicks");
-    var RightClick = false;
-    BottomLabel3.addEventListener("mousedown", BL3MD);
-    BottomLabel3.addEventListener("mouseup", BL3MU);
-    BottomLabel3.style.cursor = "pointer";
-    window.addEventListener("resize", EndGameResize);
+    //Variable Usuage
     MiddleLabelTwo.innerHTML += seconds;
     MiddleLabelThree.innerHTML += accurateclicks;
     MiddleLabelFour.innerHTML += inaccurateclicks;
     MiddleLabelFive.innerHTML += (parseInt(seconds) 
         + parseInt(accurateclicks) 
         + parseInt(inaccurateclicks));
+    //Events
+    BottomLabel3.addEventListener("mousedown", BL3MD);
+    BottomLabel3.addEventListener("mouseup", BL3MU);
+    BottomLabel3.style.cursor = "pointer";
+    window.addEventListener("resize", EndGameResize);
     window.addEventListener("contextmenu", EndGameContextMenu);
+    if(windowheight <= 600){
+        EndGameContainer.style.height = "600px";
+    }
+    if(windowwidth <= 800){
+        EndGameContainer.style.width = "800px";
+    }
     function EndGameContextMenu(e){
         if(e.which == 3 || e.button == 2){
             e.preventDefault();
         }
     }
     function BL3MD(e){
-        if(e.which == 3 || e.button == 2){
-            e.preventDefault();
-            RightClick = true
-        }
-        else{
+        if(e.which == 1 || e.button == 0){
             randomcolor = colors[Math.floor(Math.random()*colors.length)];
             BottomLabel3.style.color = randomcolor;
             RightClick = false;
         }
     }
-    function BL3MU(){
-        if(RightClick == false){
+    function BL3MU(e){
+        if(e.which == 1 || e.button == 0){
             MiddleLabelTwo.innerHTML = "Time (Seconds): "
             MiddleLabelThree.innerHTML = "Accurate Clicks: "
             MiddleLabelFour.innerHTML = "Inaccurate Clicks: "
             MiddleLabelFive.innerHTML = "Score: "
             window.location.href = "Game.html";
+            GameLoad();
         }
-    }
-    if(windowheight < 600)
-    {
-        EndGameContainer.style.height = "600px";
-    }
-    if(windowwidth < 800)
-    {
-        EndGameContainer.style.width = "800px";
     }
     function EndGameResize(){
         windowheight = window.innerHeight;
         windowwidth = window.innerWidth;
-        if(windowheight <= 600)
-        {
+        if(windowheight <= 600){
             EndGameContainer.style.height = "600px";
-        }
-        if(windowheight > 600)
-        {
+        } else if(windowheight > 600){
             EndGameContainer.style.height = "100%";  
         }
-        if(windowwidth <= 800)
-        {
+        if(windowwidth <= 800){
             EndGameContainer.style.width = "800px";
-        }
-        if(windowwidth > 800)
-        {
+        }else if(windowwidth > 800){
             EndGameContainer.style.width = "100%";  
         }
     }
